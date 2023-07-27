95dc66152ab794cf472f72daf793ed1f01df3986
Leo Moon <jm2239@cornell.edu>
2023-07-26 17:54:12 -0700

    added some information for triggering

--- connect_AFG_31000.m
+++ connect_AFG_31000.m
@@ -10,5 +10,10 @@ v.write("FREQUENCY 10E3");
 v.write("VOLTAGE:AMPLITUDE 3.00");
 v.write("VOLTAGE:OFFSET 1.00");
 v.write("PHASE:ADJUST 0DEG");
-v.writeread("TRIG:SOUR?")
-
+v.write("TRIG:SOUR EXT")
+is_trigger_on = v.writeread("TRIG:SOUR?");
+v.write("TRIG:SLOP POS");
+is_trigger_pos = v.writeread("TRIG:SLOP?");
+fprintf(is_trigger_on);
+fprintf(is_trigger_pos);
+%% I NEED TO LOOK AT THE SEQuence function and see how that relates to triggering
