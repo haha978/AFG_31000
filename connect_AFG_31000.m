c6efe743fd7a33e6dee452bc7d71e49d1d770895
Leo Moon <jm2239@cornell.edu>
2023-07-27 13:11:37 -0700

    working trigger for 100 cycle of arbitrary pulse

--- connect_AFG_31000.m
+++ connect_AFG_31000.m
@@ -3,17 +3,25 @@ close;
 % When I query, I get "1" if the query is
 v = visadev("USB0::0x0699::0x0355::C019986::INSTR");
 % Maybe need to call this only once: v.write("*RST");
-v.write("OUTP1:STAT ON");
-is_on = v.writeread("OUTP1:STAT?");
-v.write("FUNCTION SQU");
-v.write("FREQUENCY 10E3");
-v.write("VOLTAGE:AMPLITUDE 3.00");
-v.write("VOLTAGE:OFFSET 1.00");
-v.write("PHASE:ADJUST 0DEG");
-v.write("TRIG:SOUR EXT")
+v.write("TRIG:SOUR EXT");
 is_trigger_on = v.writeread("TRIG:SOUR?");
 v.write("TRIG:SLOP POS");
 is_trigger_pos = v.writeread("TRIG:SLOP?");
+v.write("SOURce:BURSt:INFInite:REARm");
+burstmode = v.writeread("SOURce1:BURSt:MODE?");
+v.write("SOURce1:BURSt:NCYCles 100");
+n_cycles = v.writeread("SOURce1:BURSt:NCYCles?");
 fprintf(is_trigger_on);
-fprintf(is_trigger_pos);
+fprintf(burstmode);
+fprintf(n_cycles);
+v.write("OUTP1:STAT ON");
+is_on = v.writeread("OUTP1:STAT?");
+v.write("FUNCTION SIN");
+v.write("FREQUENCY 3E3");
+v.write("VOLTAGE:AMPLITUDE 3.00");
+v.write("VOLTAGE:OFFSET 1.00");
+v.write("PHASE:ADJUST 0DEG");
+
+
+
 %% I NEED TO LOOK AT THE SEQuence function and see how that relates to triggering
